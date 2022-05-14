3f380cf202c42180ba41518166a9c3cc1577deca
rajatgautam755421 <rg755421@gmail.com>
2022-05-14 11:18:07 +0545

    sjkhdkj

--- Routes/newsletter.js
+++ Routes/newsletter.js
@@ -6,14 +6,18 @@ router.post("/newsletter", newsletter.postNewsletter);
 router.get("/newsletter", newsletter.getNewsletter);
 router.delete("/newsletter/:id", newsletter.deleteNewsletter);
 
-router.post("/notification", async (req, res) => {
-  const response = await NotificationModel.create(req.body);
-  res.json(response);
-});
-
 router.get("/notification", async (req, res) => {
   const response = await NotificationModel.find({});
   res.json(response);
+});
+
+router.put("/notification", async (req, res) => {
+  const response = await NotificationModel.find({});
+  try {
+    const firstValue = response[0];
+    firstValue.notification = true;
+    res.send("Update Successfull");
+  } catch (error) {}
 });
 
 module.exports = router;
