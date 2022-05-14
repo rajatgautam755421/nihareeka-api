a7126bc6f9de40ba55e7602137c638675df1bf62
rajatgautam755421 <rg755421@gmail.com>
2022-05-14 11:10:32 +0545

    sjkhdkj

--- Routes/newsletter.js
+++ Routes/newsletter.js
@@ -1,8 +1,14 @@
 const router = require("express").Router();
 const newsletter = require("../Controller/newsletter");
+const NotificationModel = require("../Model/Notification");
 
 router.post("/newsletter", newsletter.postNewsletter);
 router.get("/newsletter", newsletter.getNewsletter);
 router.delete("/newsletter/:id", newsletter.deleteNewsletter);
+
+router.get("/notification", async (req, res) => {
+  const response = await NotificationModel.find({});
+  res.json(response);
+});
 
 module.exports = router;
