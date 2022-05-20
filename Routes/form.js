a5f47bc06ff47cd4dad5fad4be7fcde459595866
rajatgautam755421 <rg755421@gmail.com>
2022-05-20 10:38:20 +0545

    sjkhdkj

--- Routes/form.js
+++ Routes/form.js
@@ -1,7 +1,32 @@
 const Form = require("../Model/Form");
+const nodemailer = require("nodemailer");
 
 const router = require("express").Router();
 
+function sendMail(email, message) {
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "gautamrajat185@gmail.com",
+      pass: "1462962708",
+    },
+  });
+
+  var mailOptions = {
+    from: "gautamrajat185@gmail.com",
+    to: `${email}`,
+    subject: "Mail From Nihareeka College",
+    text: `${message}`,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+}
 router.post("/form", async (req, res) => {
   const response = await Form.create(req.body);
   try {
@@ -51,6 +76,17 @@ router.delete("/form/delete/:id/", async (req, res) => {
   const response = await Form.findByIdAndRemove({ _id: id });
   try {
     res.json(response);
+  } catch (error) {
+    res.json(error.message);
+  }
+});
+
+router.get("/mail/accept/:email/:message", async (req, res) => {
+  const { email } = req.params;
+  const { message } = req.params;
+  try {
+    sendMail(email, message);
+    res.json("Email Sent Successfully");
   } catch (error) {
     res.json(error.message);
   }
